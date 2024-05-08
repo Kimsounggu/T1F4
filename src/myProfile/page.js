449ad5d04cfa607f428b76923d1d2c921b82fbd6
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2024-05-09 04:56:45 +0900

    update: 주석 일부 삭제

--- src/myProfile/page.js
+++ src/myProfile/page.js
@@ -38,6 +38,3 @@ function displayBookmarkedMovies() {
     }
   }
 }
-
-// const bookmark = document.getElementById("user-bookmark");
-// const reviewCollection = document.getElementById("user-review-collection");
